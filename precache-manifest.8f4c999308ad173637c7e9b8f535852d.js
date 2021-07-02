530b8fcef578a598379e220de1097c98db8bb81f
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-02 17:52:45 -0400

    Updates

---
+++ precache-manifest.8f4c999308ad173637c7e9b8f535852d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9604ab240aaa9d8d933ec54b5795f5",
+    "url": "/reactBirthdayReminderApp/index.html"
+  },
+  {
+    "revision": "eb5c64cd3aacdf7d6f3f",
+    "url": "/reactBirthdayReminderApp/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/reactBirthdayReminderApp/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactBirthdayReminderApp/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb5c64cd3aacdf7d6f3f",
+    "url": "/reactBirthdayReminderApp/static/js/main.bff084d3.chunk.js"
+  },
+  {
+    "revision": "530c0dfd82e944c91c9a",
+    "url": "/reactBirthdayReminderApp/static/js/runtime-main.c23ec7b6.js"
+  }
+]);
